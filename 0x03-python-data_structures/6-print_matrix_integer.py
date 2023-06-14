e612827862ba760a06be6768c777f460bb975a83
annacolleta <annacolletamtetwa@gmail.com>
2023-06-13 17:48:10 -0700

    list of list = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     """ function that prints a matrix of integers."""
-    for row in matrix
-    row_str = " ".join("{:d}".format(num) for num in row)
-    print(row_str)
+    for row in matrix:
+        for num in row
+    print("{:d}".format(num), end=' ')
+    print()
