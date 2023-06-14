0513ffc479e34aa1c57a835a9310fa126a13780c
annacolleta <annacolletamtetwa@gmail.com>
2023-06-13 19:05:38 -0700

    only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 10-divisible_by_2.py
+
+
+def divisible_by_2(my_list=[]):
+    """ function that finds all multiples of 2 in a list."""
+    reslut = []
+    for nim in my_list:
+        if num % == 0:
+            result.append(True)
+        else:
+            result.append(False)
+            return (result)
