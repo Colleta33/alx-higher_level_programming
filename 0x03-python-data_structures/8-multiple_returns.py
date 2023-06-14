71a9e7c603697351d4eeac4e45dc8768eef20e9f
annacolleta <annacolletamtetwa@gmail.com>
2023-06-13 18:43:53 -0700

    more retuns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 8-multiple_returns.py
+
+
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == '':
+        return (len(sentence), sentence[0])
+    else:
+        return (None, None)
