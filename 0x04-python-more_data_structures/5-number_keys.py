08278f688007aad7e721caf66d28a86ed544f51b
annacolleta <annacolletamtetwa@gmail.com>
2023-06-14 21:00:58 -0700

    key dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!?usr?bin?python3
+#  5-number_keys.py
+
+
+def number_keys(a_dictionary):
+    list_keys = list(dictionery.keys())
+     for i in list_keys:
+         num+= 1
+         return(num)
