7e4e94ae9d06e186b53a19d26bda68fe595c8c96
annacolleta <annacolletamtetwa@gmail.com>
2023-06-14 19:57:21 -0700

    python data structers

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@
 
 def square_matrix_simple(matrix=[]):
     """ computes the square value of all integers of a matrix."""
-    result_matrix = list(map(lambda row: list(map(lambdax: x**2,row)), matrix))
+    result_matrix = list(map(lambda row: list(map(lambdax: x**2, row)), matrix))
     return (result_matrix)
