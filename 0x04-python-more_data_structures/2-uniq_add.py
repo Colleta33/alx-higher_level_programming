31410af37384d520d516e4225a7f1138e806b24a
annacolleta <annacolletamtetwa@gmail.com>
2023-06-14 20:41:33 -0700

    unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!?usr/bin/python3
+# 2-uniq_add.py
+
+
+def uniq_add(my_list=[]):
+    """ adds all unique integers in a list (only once for each integer)."""
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_liat
+    num += i
+
+    return(num)
