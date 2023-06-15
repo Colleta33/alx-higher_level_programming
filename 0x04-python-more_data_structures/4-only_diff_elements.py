41a0ea8eaaf82bc92f3d32784d8aa8b51fa5d56d
annacolleta <annacolletamtetwa@gmail.com>
2023-06-14 20:57:04 -0700

    only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pytho3
+# 4-only_diff_elements.py
+
+
+def only_diff_elements(set_1, set_2):
+    """returns a set of all elements present in only one set."""
+    return(set_1 ^ set_2)
+
