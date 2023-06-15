39d57d09744da910568a3e73f358fdaf2fdff35b
annacolleta <annacolletamtetwa@gmail.com>
2023-06-14 20:24:41 -0700

    squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,9 +4,9 @@
 
 def square_matrix_simple(matrix=[]):
     """ computes the square value of all integers of a matrix."""
-    result_matrix =[[0] * len(matrix[0])for_in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            result_matrix[i][j] = matrix[i][j] ** 2
+    new_matrix = matrix.copy
 
-            return (result_matrix)
+    for i in range(len(matix)):
+        new_matrix[i]  list(map(lambda x: x**2, matrix[i]))
+
+        return(new_matrix)
