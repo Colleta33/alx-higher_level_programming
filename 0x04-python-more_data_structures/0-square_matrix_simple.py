cb69e1b166ce31646b096f7357cb0866a95e4753
annacolleta <annacolletamtetwa@gmail.com>
2023-06-16 18:12:36 -0700

    upadting

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def square_matrix_simple(matri=[]):
+def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 
 
 def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
+    uniq_add= set(my_list)
     num = 0
 
     for i in uniq_list:
