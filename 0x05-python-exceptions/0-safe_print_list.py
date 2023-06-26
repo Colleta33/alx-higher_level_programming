2b08a24457bb132fa74374307e7cdc517ab3570f
Annacolleta <annacolletamtewa.com>
2023-06-26 16:20:50 -0700

    python execptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print(my_list[i], end=" ")
+            print("{}".format(my_list[i]), end=" ")
             count += 1
     except IndexError:
         pass
