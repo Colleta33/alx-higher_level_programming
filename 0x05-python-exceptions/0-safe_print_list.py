6e10acbc4677cd133bda0788218676931aa44e71
Annacolleta <annacolletamtewa.com>
2023-06-26 16:34:28 -0700

    python exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,13 +4,13 @@
 
 def safe_print_list(my_list=[], x=0):
     """ a function that prints x elements of a list"""
-    count = 0
 
-    try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end=" ")
-            count += 1
+    count = 0
+    for i in range(x):
+        try:
+        print("{}".format(my_list[i]), end=" ")
+        count += 1
     except IndexError:
-        break
+        pass
     print()
     return (count)
